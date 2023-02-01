30eeaca8decc5909ad59bf415d8d6395156a764b
SvyatoslavGabuda <sgabuda@gmail.com>
2023-02-01 15:04:12 +0100

    css artist

--- scripts/scriptpag2Artist.js
+++ scripts/scriptpag2Artist.js
@@ -1,10 +1,10 @@
 const ulrArtist = "https://striveschool-api.herokuapp.com/api/deezer/artist";
 
 let params = new URLSearchParams(location.search);
-console.log(params);
+// console.log(params);
 
 let ourIDArtist = params.get("id");
-console.log(ourIDArtist);
+// console.log(ourIDArtist);
 
 const returnMinute = function (sec) {
   const minute = Math.floor(sec / 60);
@@ -19,12 +19,12 @@ const fetchUrlArtist = async function () {
     let res = await fetch(`${ulrArtist}/${ourIDArtist}`);
     if (res.ok) {
       const artista = await res.json();
-      // console.log(data);
+      // console.log(artista);
       let tracklist = await fetch(artista.tracklist);
-      console.log(tracklist);
+      // console.log(tracklist);
       const tracks = await tracklist.json();
-      console.log(tracks);
-      console.log(tracks.data);
+      // console.log(tracks);
+      // console.log(tracks.data);
 
       doveMettoArtista.innerHTML += `
        <div class="row pt-4 ">
@@ -46,23 +46,43 @@ const fetchUrlArtist = async function () {
             </div>
             
             <div class="row">
-              <div class="col-12">
-                <button><i class="bi bi-play-circle"></i></button>
+              <div class="col-12 bottoniArtista ">
+                <button class="play"><i class="bi bi-play-circle-fill"></i></button>
                 
-                <button>FOLLOWING</button>
+                <button class="follow">FOLLOWING</button>
                 <button><i class="bi bi-three-dots"></i></button>
               </div>
             </div>
 
             <div class="row">
+            <div class="col-7">
 
              <h2 class="text-light">Popolari</h2>
+             </div>
+             <div class="col-5">
+             <h2 class="text-light">Brani che ti piacciono</h2>
+             </div>
+
             </div>`;
 
       //   const tracks = data.tracks.data;
 
+      const contrainerTracce = document.getElementById("tracce");
+      const containerBraniPreferiti = document.getElementById("braniPreferiti");
+      containerBraniPreferiti.innerHTML += `<div class="row p-0 braniPreferiti">
+      <div class="col-3 position-relative"><img src=${artista.picture_small} alt="" />
+      <span ><i class="bi bi-check-circle-fill"></i></span></div>
+      <div class="col-9 d-flex flex-column justify-content-center">
+        <h5>Hai messo Mi piace a ${artista.nb_album} Brani</h5>
+        <p>Di ${artista.name}</p>
+      </div>
+    </div>
+
+
+      `;
+
       tracks.data.forEach((el, index) => {
-        doveMettoArtista.innerHTML += `
+        contrainerTracce.innerHTML += `
           <div class="row tracceArtista">
                 
                   <div class="col-1 d-flex justify-content-center align-items-center ">
