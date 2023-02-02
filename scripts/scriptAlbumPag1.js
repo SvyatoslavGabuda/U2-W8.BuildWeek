7dd0d3279c8a9d6845e8d1e10bbb54875a7564e8
SvyatoslavGabuda <sgabuda@gmail.com>
2023-02-02 20:00:02 +0100

    fix tutto

--- scripts/scriptAlbumPag1.js
+++ scripts/scriptAlbumPag1.js
@@ -13,6 +13,48 @@ const returnMinute = function (sec) {
   const time = `${minute}:${restSeconds}`;
   return time;
 };
+const playlistLida = document.getElementById("playlistLidia");
+const playlistNames = [
+  "Grazie a Lidia per la playlist <3",
+  "Be The Young Seasons 1-5",
+  "Be The Young Seasons 6-8",
+  "persona di m*rda (gen-feb 2023)",
+  "Musical 2022",
+  "pippo, pluto e paperino (nov-dec 2022)",
+  "early stage emily syndrome (sett-ott 2022)",
+  "Be the young",
+  "'...' cit. Kimiko (lug-ago 2022)",
+  "saggio vibes 💃🩰",
+  "main character energy (mag-giu 2022)",
+  "that fucking mood 🔪☠️",
+  "VEE, CARLOTTA E GIACOMO VANNO A BOSIO",
+  "An Emily Winchester kind of mood 🔪🖕",
+  "landing page (mar-apr 2022)",
+  "2021 lol",
+  "cosa cazzo vuol dire questa affermazione (gen-feb 2022)",
+  "honey and glass (nov-dic 2021)",
+  "(Revenge) Training Arc 🦍",
+  "Lidia 🤝 Emily",
+  "minecraft e nintendo switch (sep-oct 2021)",
+  "silvano d'orba? I hardly know her (lug - ago 2021)",
+  "Culo 2021",
+  "Frah Quintale Mix",
+  "Francesco Guccini Mix",
+  "Lo Stato Sociale Mix",
+  "chapter 4/? (mag-giu 2021)",
+  "Strive School <> The Hunt Motivation",
+  "mi stavo dimenticando (mar-apr 2021)",
+  "high school musical 1,2,3",
+  "quanto trash cazzo",
+  "The 2020 Playlist",
+  "ma(ncanza) che cazzo ne so io (gen-feb 2021)",
+];
+
+playlistNames.forEach((el) => {
+  playlistLida.innerHTML += `<li>
+  <a href="#">${el}</a>
+</li>`;
+});
 
 const caricaArtista = function (event) {
   console.log(event);
@@ -225,7 +267,7 @@ const closeFriends_btn = document.querySelector(".show-friends");
 
 const showFriends = function () {
   const aside = document.querySelector("aside");
-  aside.classList.toggle("d-lg-block");
+  aside.classList.toggle("d-xl-block");
 };
 
 closeFriends_btn.onclick = showFriends;
